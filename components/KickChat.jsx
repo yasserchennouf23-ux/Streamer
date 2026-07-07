1240bc8ce95791df17b6f271b37e12d31c3f6632
yasserchennouf23-ux <yasserchennouf23@gmail.com>
2026-07-07 11:17:12 +0100

    Update KickChat.jsx

--- components/KickChat.jsx
+++ components/KickChat.jsx
@@ -4,7 +4,7 @@ export default function KickChat() {
   return (
     <div className="w-full h-[500px] lg:h-[calc(100vh-112px)] overflow-hidden rounded-xl bg-zinc-900 border border-zinc-800 shadow-2xl shadow-black/50">
       <iframe
-        src="https://chat.kick.cx/embed/reda-3x"
+        src="https://chat.kick.cx/embed/f0nixx"
         className="w-full h-full"
         frameBorder="0"
         scrolling="no"
